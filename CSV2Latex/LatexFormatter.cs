2cbe81868c6b19185bc499be761a83b9c21ea2ee
dylnbrnt <9741097@student.swin.edu.au>
2016-11-26 10:43:34 +1100

    * .gitignore: * CSV2Latex.sln: * Program.cs: * CSV2Latex.csproj: * LatexFormatter.cs: * AssemblyInfo.cs:
    * README.md: 26/Nov changes

--- CSV2Latex/LatexFormatter.cs
+++ CSV2Latex/LatexFormatter.cs
@@ -1,6 +1,8 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+
 
 namespace CSV2Latex
 {
@@ -10,12 +12,16 @@ namespace CSV2Latex
 		private int _rowCount;
 		private int _columnCount;
 		private string _header = null;
-		private string[] _values;
+		private string _values;
 
 		public LatexFormatter ()
 		{
 		}
 
+		/// <summary>
+		/// Load the specified filename.
+		/// </summary>
+		/// <param name="filename">Filename.</param>
 		public void Load(string filename)
 		{
 			StreamReader reader = new StreamReader (filename);
@@ -29,12 +35,14 @@ namespace CSV2Latex
 				_rowCount = _inputData.Count;
 				Process();
 
-
 			}finally{
 				reader.Close ();
 			}
 		}
 
+		/// <summary>
+		/// Process the csv data into Lastex table format
+		/// </summary>
 		public void Process(){
 			//header
 			string[] headerInput = _inputData[0].Split(',');
@@ -61,30 +69,63 @@ namespace CSV2Latex
 				_header += headerInput [j] + "\n"+ "& ";;
 			}
 			//format the end of the string
-			_header = _header.Remove(_header.Length - 2, 2) +"\\\\";
+			_header = _header.Remove(_header.Length - 2, 2) +"\\\\\n\\hline";
 
 			//process the values
 			string[] valueInput;
 			//go through each row
+
 			for(int index = 1; index < _rowCount; index++)
 			{
-				valueInput = _inputData [index].Split (',');				
+				valueInput = _inputData [index].Split (',');
+				//	//add Latex table formatting
+				for (int j = 0; j < _columnCount; j++)
+				{
+					if (j == _columnCount - 1)
+					{
+						//end of line
+						_values += valueInput[j] + "\t\\\\\n\\hline\n";
+					}
+					else
+					_values += valueInput[j] + " & ";
+				}
 			}
 
 		}
 
-		public void ProcessValues()
+		/// <summary>
+		/// Save the specified filename.
+		/// </summary>
+		/// <param name="filename">Filename.</param>
+		public void Save(string filename)
 		{
-
+			string Output = Header + Values;
+			StreamWriter writer = new StreamWriter(filename);
+			try
+			{
+				writer.WriteLine(Output);
+			}
+			finally
+			{
+				writer.Close();
+			}
+			SaveToClipboard(Output);
 		}
 
+		public void SaveToClipboard(string output)
+		{
+			Thread thread = new Thread(() => Clipboard.SetText(output));
+			thread.SetApartmentState(ApartmentState.STA); //Set the thread to STA
+			thread.Start();
+			thread.Join();
+		}
 
 		public int RowCount{ get { return _rowCount; } }
 		public int ColumnCount{ get { return  _columnCount; } }
 		public List<string> InputData{ get { return _inputData; } }
 
 		public string Header{ get { return _header; } }
-		public string[] Values{ get { return _values; } }
+		public string Values{ get { return _values; } }
 	}
 }
 
--- CSV2Latex/Program.cs
+++ CSV2Latex/Program.cs
@@ -1,23 +1,41 @@
 ﻿using System;
 
+/**
+ * [...]
+ 
+ *
+ * @author  Dylan Brunet
+ * @version 2.0, 26/11/16
+ */
 namespace CSV2Latex
 {
 	class MainClass
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
-			LatexFormatter format = new LatexFormatter ();
+			Console.WriteLine ("CSV to Latex Table Format\nSaves to file and clipboard!");
+			Console.WriteLine("Dylan Brunet");
+			while(true)
+			{
+				Console.Write("File: ");
 
-			//try{
-			//string filename = Console.ReadLine();
-			format.Load ("lab4.csv");
-			Console.WriteLine("Rows: "+format.RowCount.ToString() +" Coloumns: "+format.ColumnCount.ToString());
-			Console.WriteLine(format.Header);
-			//Console.ReadLine ();
-			//}catch (Exception eFileMissing){
-			//	Console.Error.WriteLine("Error loading file: {0}", eFileMissing.Message);
-			//}
+				LatexFormatter format = new LatexFormatter();
+				try
+				{
+					string filename = Console.ReadLine();
+					format.Load(filename);
+					//format name of output file to save
+					string[] temp = filename.Split('.');
+					string outFilename = temp[0] + ".txt";
+					//Save to file
+					format.Save(outFilename);
+					Console.WriteLine("Output complete!");
+				}
+				catch (Exception eFileMissing)
+				{
+					Console.Error.WriteLine("Error loading file: {0}", eFileMissing.Message);
+				}
+			}
 		}//end main
 	}
 }
